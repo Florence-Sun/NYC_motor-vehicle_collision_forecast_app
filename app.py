b1bc660839e5ae43c131a27428b9436c5ae1a9d1
Florence Sun <sunzm.xjtu@gmail.com>
2020-03-19 13:10:59 -0400

    revised html

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ def home():
     return render_template('home.html')
 
 #route for prediction of collision number model
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
+
 
     #get text from the incoming request (submitted on predict button click)
     input_date=request.form['namequery']
@@ -44,8 +45,8 @@ def predict():
         manhattan['trend_model']=trend_model.predict(manhattan[['Julian', 'const']])
         manhattan['trend_model_error'] = manhattan['CRASH NUMBER'] - manhattan['trend_model']
         features_for_season_model=manhattan[['trend_model_error', 'sin(week)', \
-                                        'cos(week)', 'sin(biweek)', 'cos(biweek)', \
-                                        'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
+                                            'cos(week)', 'sin(biweek)', 'cos(biweek)', \
+                                            'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
         manhattan['Predicted_error_value'] = season_model.predict(features_for_season_model)
         manhattan['PREDICT NUMBER'] = manhattan['trend_model'] + manhattan['Predicted_error_value']
         manhattan_prediction=round(manhattan['PREDICT NUMBER'].values[0])
@@ -54,8 +55,8 @@ def predict():
         brooklyn['trend_model'] = trend_model.predict(brooklyn[['Julian', 'const']])
         brooklyn['trend_model_error'] = brooklyn['CRASH NUMBER'] - brooklyn['trend_model']
         features_for_season_model_bro = brooklyn[['trend_model_error', 'sin(week)', \
-                                               'cos(week)', 'sin(biweek)', 'cos(biweek)', \
-                                               'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
+                                                   'cos(week)', 'sin(biweek)', 'cos(biweek)', \
+                                                   'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
         brooklyn['Predicted_error_value'] = season_model.predict(features_for_season_model_bro)
         brooklyn['PREDICT NUMBER'] = brooklyn['trend_model'] + brooklyn['Predicted_error_value']
         brooklyn_prediction = round(brooklyn['PREDICT NUMBER'].values[0])
@@ -64,8 +65,8 @@ def predict():
         queens['trend_model'] = trend_model.predict(queens[['Julian', 'const']])
         queens['trend_model_error'] = queens['CRASH NUMBER'] - queens['trend_model']
         features_for_season_model_que = queens[['trend_model_error', 'sin(week)', \
-                                               'cos(week)', 'sin(biweek)', 'cos(biweek)', \
-                                               'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
+                                                   'cos(week)', 'sin(biweek)', 'cos(biweek)', \
+                                                   'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
         queens['Predicted_error_value'] = season_model.predict(features_for_season_model_que)
         queens['PREDICT NUMBER'] = queens['trend_model'] + queens['Predicted_error_value']
         queens_prediction = round(queens['PREDICT NUMBER'].values[0])
@@ -74,8 +75,8 @@ def predict():
         staten_island['trend_model'] = trend_model.predict(staten_island[['Julian', 'const']])
         staten_island['trend_model_error'] = staten_island['CRASH NUMBER'] - staten_island['trend_model']
         features_for_season_model_stan = staten_island[['trend_model_error', 'sin(week)', \
-                                                'cos(week)', 'sin(biweek)', 'cos(biweek)', \
-                                                'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
+                                                    'cos(week)', 'sin(biweek)', 'cos(biweek)', \
+                                                    'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
         staten_island['Predicted_error_value'] = season_model.predict(features_for_season_model_stan)
         staten_island['PREDICT NUMBER'] = staten_island['trend_model'] + staten_island['Predicted_error_value']
         staten_island_prediction = round(staten_island['PREDICT NUMBER'].values[0])
@@ -84,27 +85,27 @@ def predict():
         bronx['trend_model'] = trend_model.predict(bronx[['Julian', 'const']])
         bronx['trend_model_error'] = bronx['CRASH NUMBER'] - bronx['trend_model']
         features_for_season_model_brox = bronx[['trend_model_error', 'sin(week)', \
-                                               'cos(week)', 'sin(biweek)', 'cos(biweek)', \
-                                               'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
+                                                   'cos(week)', 'sin(biweek)', 'cos(biweek)', \
+                                                   'sin(biyear)', 'cos(biyear)', 'sin(year)', 'cos(year)']]
         bronx['Predicted_error_value'] = season_model.predict(features_for_season_model_brox)
         bronx['PREDICT NUMBER'] = bronx['trend_model'] + bronx['Predicted_error_value']
         bronx_prediction = round(bronx['PREDICT NUMBER'].values[0])
 
 
         return render_template('home.html', input_date=input_date, \
-                               manhattan_prediction=manhattan_prediction, \
-                               brooklyn_prediction=brooklyn_prediction, \
-                               queens_prediction=queens_prediction, \
-                               staten_island_prediction=staten_island_prediction, \
-                               bronx_prediction=bronx_prediction)
+                                   manhattan_prediction=manhattan_prediction, \
+                                   brooklyn_prediction=brooklyn_prediction, \
+                                   queens_prediction=queens_prediction, \
+                                   staten_island_prediction=staten_island_prediction, \
+                                   bronx_prediction=bronx_prediction)
     else:
         error_mesg="The input date is wrong. Please input any day from last week."
         return render_template('home.html', input_date=input_date, \
-                               manhattan_prediction=error_mesg, \
-                               brooklyn_prediction=error_mesg, \
-                               queens_prediction=error_mesg, \
-                               staten_island_prediction=error_mesg, \
-                               bronx_prediction=error_mesg)
+                                   manhattan_prediction=error_mesg, \
+                                   brooklyn_prediction=error_mesg, \
+                                   queens_prediction=error_mesg, \
+                                   staten_island_prediction=error_mesg, \
+                                   bronx_prediction=error_mesg)
 @app.route("/about")
 def about():
     return render_template('about.html')
